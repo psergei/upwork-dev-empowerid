96e0c5b8a335c8ab197f17161b139120c94e3b4e
Sergiy Prognimak <sergei@prognimak.com>
2023-09-02 16:52:34 +0200

    Authority fix

--- demo/src/auth-config.js
+++ demo/src/auth-config.js
@@ -1,7 +1,7 @@
 export const authConfig = {
   auth: {
     clientId: '0a66b744-2412-4f03-82a2-b8bfdcb6ddc2',
-    authority: 'https://sergeiprognimak.ciamlogin.com/',
+    authority: 'https://login.microsoftonline.com/0281b8d2-65ba-4fe1-93b0-3877b20be548',
     redirectUri: '/',
     postLogoutRedirectUri: '/'
   },
